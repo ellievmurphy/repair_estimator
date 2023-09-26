f4cf6270a9936a277c97a7b8c2716a0d077c5af7
evmurphy <evmurphy@ncsu.edu>
2023-09-26 09:38:04 -0400

    [Implementation] Integrated NavigationStacks + paths. Need to create tests and re-structure the categories / subcategories

---
+++ Repair Estimator/estimator/App/PropertyManager/IntroductoryView.swift
@@ -0,0 +1,20 @@
+//
+//  IntroductoryView.swift
+//  estimator
+//
+//  Created by Ellie Murphy on 9/15/23.
+//
+
+import SwiftUI
+
+struct IntroductoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntroductoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroductoryView()
+    }
+}
